df593ebc7dd00c4c4061c20a9d78409ea44fd7ed
Muznah Aftab <muznah.a@gmail.com>
2018-12-24 19:22:43 +0530

    Create A Linked ist

---
+++ Linked_List/insertion.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+struct node
+{
+	int data;
+	struct node* next;
+}*start=NULL;
+
+struct node* insert ( struct node* start )
+{
+	struct node* new_node;
+	int val;
+	cout<<"Enter element to insert: ";
+	cin>>val;
+	while(val!= -1)
+	{
+		new_node = (struct node*)malloc(sizeof(struct node));
+		new_node->data = val;
+		if(start == NULL)
+		{
+			start = new_node;
+			new_node->next = NULL;
+		}
+		struct node* ptr = start;
+		while(ptr->next != NULL)
+			ptr = ptr->next;
+		ptr->next = new_node;
+		new_node-> next = NULL;
+		cout<<"Enter next value (Press -1 to terminate) : ";
+		cin>>val;
+	}
+	return start;
+}
+
+void display( struct node* start)
+{
+	struct node* ptr = start;
+	while( ptr != NULL)
+	{	
+		cout<<ptr->data<<"  ";
+		ptr = ptr->next;
+	}
+}
+
+int main()
+{
+	start = insert(start);
+	display(start);
+}
